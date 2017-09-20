aef6321f0f8490eb3d07dca4443e5be25886c7ab
DominWos <dominik.wosinski@gmail.com>
2017-09-20 13:12:42 +0200

    Initial Commit

---
+++ src/main/scala/Boot.scala
@@ -0,0 +1,5 @@
+object Boot extends App {
+  override def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
---
+++ src/main/scala/ChessPiece.scala
@@ -0,0 +1,31 @@
+
+sealed trait ChessPiece{
+
+  def sortingPriority: Int
+}
+
+case object King extends ChessPiece {
+
+  override def sortingPriority = 5
+}
+
+case object Queen extends ChessPiece {
+
+  override def sortingPriority = 1
+}
+
+case object Rook extends ChessPiece {
+
+  override def sortingPriority = 3
+}
+
+case object Bishop extends ChessPiece {
+
+  override def sortingPriority = 2
+}
+
+case object Knight extends ChessPiece {
+  
+  override def sortingPriority = 4
+}
+
