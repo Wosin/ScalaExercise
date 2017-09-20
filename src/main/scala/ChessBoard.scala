d23cddd544359220f7b010e7e5a4ae62fbeb7027
Dominik Wosinski <dominik.wosinski@huuugegames.com>
2017-09-20 13:48:42 +0200

    Added ChessBoard class and companion object

---
+++ src/main/scala/ChessBoard.scala
@@ -0,0 +1,14 @@
+
+object ChessBoard {
+  def apply(width:Int, height:Int) : ChessBoard = {
+    val chessFields = for {
+      x <- 1 to width
+      y <- 1 to height
+    } yield ChessField(x, y)
+
+    new ChessBoard(chessFields.toSet)
+  }
+}
+case class ChessBoard(freeFields: Set[ChessField], piecesOnFields: Map[ChessField, ChessPiece] = Map()) {
+
+}
