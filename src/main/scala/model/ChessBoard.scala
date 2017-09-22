8bce23cf2de1f18ccc4371cff4975336804f39f5
DominWos <dominik.wosinski@gmail.com>
2017-09-22 08:45:39 +0200

    Added toString for ChessBoard and ChessPieces

--- src/main/scala/model/ChessBoard.scala
+++ src/main/scala/model/ChessBoard.scala
@@ -8,9 +8,31 @@ object ChessBoard {
       y <- 1 to height
     } yield ChessField(x, y)
 
-    new ChessBoard(chessFields.toSet)
+    new ChessBoard(width, height, chessFields.toSet)
   }
 }
-case class ChessBoard(freeFields: Set[ChessField], piecesOnFields: Map[ChessField, ChessPiece] = Map()) {
+case class ChessBoard(width: Int, height: Int, freeFields: Set[ChessField], piecesOnFields: Map[ChessField, ChessPiece] = Map()) {
+  override def toString: String = {
+   val string =  for {
+    y <- 1 to height
+    x <- 1 to width
 
+  } yield {
+     val str = piecesOnFields.find(
+       entry => entry._1.x == x && entry._1.y == y
+     ) match {
+       case Some(piece) => piece._2.toString()
+       case _ => " "
+     }
+
+     if(x == width){
+       "|"+ str + "|\n"
+
+     } else {
+       "|" + str
+     }
+   }
+
+    string.mkString
+  }
 }
